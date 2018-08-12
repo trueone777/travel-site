71966a7d7ccf81f7bf69fa67835d7bbd3ffee547
Pawel Rabinovic <trueone777@googlemail.com>
2018-08-13 00:05:39 +0200

    Completed lazyloading of images.

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11200,6 +11200,7 @@ class RevealOnScroll {
 
 class StickyHeader {
     constructor() {
+        this.lazyImages = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".lazyload");
         this.siteHeader = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".site-header");
         this.headerTriggerElement = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".large-hero__title");
         this.createHeaderWaypoint();
@@ -11207,6 +11208,13 @@ class StickyHeader {
         this.headerLinks = __WEBPACK_IMPORTED_MODULE_0_jquery___default()(".primary-nav a");
         this.createPageSectionWaypoints();
         this.addSmoothScrolling();
+        this.refreshWaypoints();
+    }
+
+    refreshWaypoints() {
+        this.lazyImages.on('load', function () {
+            Waypoint.refreshAll();
+        });
     }
 
     addSmoothScrolling() {
