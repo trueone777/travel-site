b46be30238ebf378f02f7db28757f1d3ed6a9051
Pawel Rabinovic <trueone777@googlemail.com>
2018-08-14 02:09:40 +0200

    Completed gulp build task.

---
+++ gulp/tasks/build.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    del =require('del')
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cssnano = require('gulp-cssnano'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', () => {
+
+    browserSync.init({
+
+        notify: false,
+        server: {
+            baseDir: "docs"
+        }
+    })
+});
+
+gulp.task('deleteDistFolder', ['icons'], () => {
+    return del('./docs');
+});
+
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], () => {
+    const pathsToCopy = [
+        './app/**/*',
+        '!./app/index.html',
+        '!./app/assets/images/**',
+        '!./app/assets/styles/**',
+        '!./app/assets/scripts/**',
+        '!./app/temp',
+        '!./app/temp/**'
+    ]
+
+    return gulp.src(pathsToCopy)
+        .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('optimizeImages', ['deleteDistFolder'], () => {
+    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            multipass: true
+        }))
+        .pipe(gulp.dest("./docs/assets/images"));
+});
+
+gulp.task('useminTrigger', ['deleteDistFolder'], () => {
+    gulp.start('usemin');
+});
+
+gulp.task('usemin', ['styles', 'scripts'], () => {
+    return gulp.src('./app/index.html')
+        .pipe(usemin({
+            css: [() => {return rev()}, () => {return cssnano()}],
+            js: [() => {return rev()}]
+        }))
+        .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
